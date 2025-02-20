7b7de4ead9366f13c1175bcab2c22feb72e53799
Nathalia <nathaliabeatriz3101@gmail.com>
2025-02-20 13:17:14 -0300

    Ajustes

--- medicamento.php
+++ medicamento.php
@@ -10,33 +10,5 @@
             	$pag = new CadastrarMedicamento();
 			}
         }
-
-        // } elseif($fun == "listar"){
-			
-		// 	include_once("controle/ListarCurso_class.php");
-		// 	$pag = new ListarCurso();
-			
-		// }elseif($fun == "alterar"){
-			
-		// 	include_once("controle/AlterarCurso_class.php");
-		// 	$pag = new AlterarCurso();
-			
-		// } elseif($fun == "excluir"){
-			
-		// 	include_once("controle/ExcluirCurso_class.php");
-		// 	$pag = new ExcluirCurso();
-			
-		// }  elseif($fun == "exibir") {
-		// 	include_once("controle/ExibirCurso_class.php");
-		// 	$pag = new ExibirCurso();
-			
-		// } else {
-		// 	include_once("controle/ListarCurso_class.php");
-		// 	$pag = new ListarCurso();			
-		// }
     }
-	// }else {
-	// 	include_once("controle/ListarCurso_class.php");
-	// 	$pag = new ListarCurso();		
-    // }
 ?>
--- solicitacao.php
+++ solicitacao.php
@@ -41,32 +41,5 @@
 				}
 			}
 		}
-        // } elseif($fun == "listar"){
-			
-		// 	include_once("controle/ListarCurso_class.php");
-		// 	$pag = new ListarCurso();
-			
-		// }elseif($fun == "alterar"){
-			
-		// 	include_once("controle/AlterarCurso_class.php");
-		// 	$pag = new AlterarCurso();
-			
-		// } elseif($fun == "excluir"){
-			
-		// 	include_once("controle/ExcluirCurso_class.php");
-		// 	$pag = new ExcluirCurso();
-			
-		// }  elseif($fun == "exibir") {
-		// 	include_once("controle/ExibirCurso_class.php");
-		// 	$pag = new ExibirCurso();
-			
-		// } else {
-		// 	include_once("controle/ListarCurso_class.php");
-		// 	$pag = new ListarCurso();			
-		// }
     }
-	// }else {
-	// 	include_once("controle/ListarCurso_class.php");
-	// 	$pag = new ListarCurso();		
-    // }
 ?>
